d51b24af17be2e89992f4e5b79b0f276617edee3
Ignacio Calvo <ignaciocalvo@live.com.ar>
2024-08-24 16:50:44 -0300

    Improving CustomerIdentity API to retrieve not only the Token but also the Customer information upon succesfull login or registration so then Front end can now leverage that to improve login and as part of that store the information of the logged in Customer to store it within the context.

--- POS.CustomerIdentity.API/Models/LoginResult.cs
+++ POS.CustomerIdentity.API/Models/LoginResult.cs
@@ -1,9 +1,12 @@
-﻿namespace POS.CustomerIdentity.API.Models
+﻿using POS.CustomerIdentity.API.DTOs;
+
+namespace POS.CustomerIdentity.API.Models
 {
     public class LoginResult
     {
         public bool IsSuccess { get; set; }
         public string Token { get; set; }
         public string ErrorMessage { get; set; }
+        public CustomerDto Customer { get; set; }
     }
 }
